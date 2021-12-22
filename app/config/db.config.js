6cf0caba21af60eb3399458691a30c576bfa21a2
subastianSk <subastian.sk@gmail.com>
2021-12-21 21:57:51 -0500

    saa

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "sql6.freemysqlhosting.net",
+  USER: "sql6460255",
+  PASSWORD: "H2Q4QzWi4w",
+  DB: "sql6460255",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
